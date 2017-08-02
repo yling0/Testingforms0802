bfe8676176cd25aacb9414ee0355cb38bfa0579a
yling0 <xyun2016@gmail.com>
2017-08-02 11:51:19 -0400

    first commit

---
+++ src/main/java/me/yling/testingform0802/Testingform0802Application.java
@@ -0,0 +1,12 @@
+package me.yling.testingform0802;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testingform0802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testingform0802Application.class, args);
+	}
+}
